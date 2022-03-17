41d5f0d72290f08b14014855f24ab024e7dbdb46
Kevin_Florio <kevinf@3dinfotech.com>
2022-03-17 09:01:10 -0700

    Initial push

---
+++ ConnectionTester/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace ConnectionTester
+{
+    partial class FrmConnectionTester
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmConnectionTester));
+            this.lblIP = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(20, 20);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(28, 20);
+            this.lblIP.TabIndex = 0;
+            this.lblIP.Text = "IP:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(24, 99);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(94, 35);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(787, 99);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 35);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(124, 99);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(94, 35);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(54, 20);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(164, 26);
+            this.txtIP.TabIndex = 5;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(24, 140);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(857, 450);
+            this.txtOutput.TabIndex = 6;
+            // 
+            // FrmConnectionTester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 602);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblIP);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmConnectionTester";
+            this.Text = "Connection Tester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ ConnectionTester/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnectionTester
+{
+    public partial class FrmConnectionTester : Form
+    {
+        private CancellationTokenSource src;
+        private CancellationToken token;
+        public const int TimeOut = 100;
+
+        public FrmConnectionTester()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            src = new CancellationTokenSource();
+            token = src.Token;
+            btnStart.Enabled = false;
+            try
+            {
+                string ipadd = txtIP.Text.Trim();
+                if (string.IsNullOrWhiteSpace(ipadd) || !IPAddress.TryParse(ipadd, out IPAddress addr))
+                {
+                    MessageBox.Show($"Invalid IP: {ipadd}");
+                }
+                else
+                {
+                    Task.Run(() => TestConnection(token, ipadd), token);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Exception: {ex.Message}");
+                btnStart.Enabled = true;
+            }
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                src.Cancel();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Exception: {ex.Message}");
+            }
+            finally
+            {
+                btnStart.Enabled = true;
+            }
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = string.Empty;
+        }
+
+        public void TestConnection(CancellationToken ct, string IP)
+        {
+            long num = 0;
+            while (!ct.IsCancellationRequested)
+            {
+                if (!PingHost(IP))
+                {
+                    Log($"Ping Failed for IP: {IP}");
+                }
+                else if (num % 10 == 0)
+                {
+                    Log("Success.");
+                }
+
+                if (num == long.MaxValue)
+                {
+                    num %= 10;
+                }
+                ++num;
+                Thread.Sleep(100);
+            }
+        }
+
+        /// <summary>
+        /// Pings host
+        /// </summary>
+        /// <param name="nameOrAddress">IP Address of Device</param>
+        /// <returns>True/False == pingable</returns>
+        public static bool PingHost(string nameOrAddress)
+        {
+            bool pingable = false;
+            Ping pinger = null;
+
+            try
+            {
+                pinger = new Ping();
+                PingReply reply = pinger.Send(nameOrAddress, TimeOut);
+                pingable = reply.Status == IPStatus.Success;
+            }
+            catch (PingException ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            finally
+            {
+                if (pinger != null)
+                {
+                    pinger.Dispose();
+                }
+            }
+
+            return pingable;
+        }
+
+        public void Log(string message)
+        {
+            if (this.InvokeRequired)
+            {
+                Invoke(new Action(() =>
+                {
+                    txtOutput.AppendText(DateTime.Now + ": " + message + Environment.NewLine);
+                }));
+            }
+            else
+            {
+                txtOutput.AppendText(DateTime.Now + ": " + message + Environment.NewLine);
+            }
+        }
+    }
+}
